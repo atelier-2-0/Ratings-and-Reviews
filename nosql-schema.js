71a606ed999cbf937ac414fa2db98fa1a166ef3e
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-11-16 17:24:29 -0800

    Add data validation options

--- nosql-schema.js
+++ nosql-schema.js
@@ -1,47 +1,134 @@
 const mongoose = require('mongoose');
 
+// code to connect to db not added
+
 const reviewSchema = new mongoose.Schema({
-  review_id: Number,
-  data: Date,
-  rating: Number,
-  summary: String,
-  body: String,
-  recommend: Boolean,
-  helpfulness: Number,
-  response: String,
-  reviewer_name: String,
-  reviewer_email: String,
+  review_id: {
+    type: Number,
+    required: true
+  },
+  data: {
+    type: Date,
+    default: Date.now,
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true
+  },
+  summary: {
+    type: String,
+    maxLength: 60,
+    required: true
+  },
+  body: {
+    type: String,
+    maxLength: 1000,
+    required: true
+  },
+  recommend: {
+    type: Boolean,
+    required: true
+  },
+  helpfulness: {
+    type: Number,
+    required: false
+  },
+  response: {
+    type: String,
+    default: null,
+    required: false
+  },
+  reviewer_name: {
+    type: String,
+    required: true
+  },
+  reviewer_email: {
+    type: String,
+    required: true
+  },
   photos: [{
-    photo_id: Number,
-    url: String,
+    photo_id: {
+      type: Number,
+      required: false
+    },
+    url: {
+      type: String,
+      required: false
+    },
   }]
 });
 
 const reviewListSchema = new mongoose.Schema({
-  product_id: Number,
+  product_id: {
+    type: Number,
+    required: true
+  },
   reviews: [reviewSchema],
   metadata: {
-    recommended: Boolean,
+    recommended: {
+      true: {
+        type: Number,
+        required: false
+      },
+      false: {
+        type: Number,
+        required: false
+      }
+    },
     characteristics: {
       size: {
-        id: Number,
-        value: Number,
+        id: {
+          type: Number,
+          required: false
+        },
+        value: {
+          type: Number,
+          required: false
+        },
       },
       width: {
-        id: Number,
-        value: Number,
+        id: {
+          type: Number,
+          required: false
+        },
+        value: {
+          type: Number,
+          required: false
+        },
       },
       comfort: {
-        id: Number,
-        value: Number,
+        id: {
+          type: Number,
+          required: false
+        },
+        value: {
+          type: Number,
+          required: false
+        },
       },
     },
     ratings: {
-      1: Number,
-      2: Number,
-      3: Number,
-      4: Number,
-      5: Number,
+      1: {
+        type: Number,
+        required: false
+      },
+      2: {
+        type: Number,
+        required: false
+      },
+      3: {
+        type: Number,
+        required: false
+      },
+      4: {
+        type: Number,
+        required: false
+      },
+      5: {
+        type: Number,
+        required: false
+      },
     }
   },
 });
