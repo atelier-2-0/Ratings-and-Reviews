aa6a3400b7a69270c5fa685099da8901dc22b336
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-11-20 15:41:24 -0800

    Add callbacks for controller code

--- server/controllers/reviewsController.js
+++ server/controllers/reviewsController.js
@@ -8,21 +8,46 @@ import {
   from '../model/reviewsModel.js';
 
 export const getReviews = (req, res) => {
-  listReviews(req.params);
+  listReviews(req.params, (err, results) => {
+    if (err) {
+      console.log('error getting reviews...', err);
+    }
+    res.send(results);
+  });
 };
 
 export const getMetadata = (req, res) => {
-  retrieveMetadata(req.params);
+  retrieveMetadata(req.params, (err, results) => {
+    if (err) {
+      console.log('error getting metadata...', err);
+    }
+    res.send(results);
+  });
 };
 
 export const postReview = (req, res) => {
-  addReview(req.body);
+  addReview(req.body, (err, results) => {
+    if (err) {
+      console.log('error posting review...', err);
+    }
+    res.send(results);
+  });
 };
 
 export const markAsHelpful = (req, res) => {
-  updateHelpfulness(req.params);
+  updateHelpfulness(req.params, (err, results) => {
+    if (err) {
+      console.log('error marking review as helpful...', err);
+    }
+    res.send(results);
+  });
 };
 
 export const reportReview = (req, res) => {
-  markAsReported(req.params);
+  markAsReported(req.params, (err, results) => {
+    if (err) {
+      console.log('error reporting review...', err);
+    }
+    res.send(results);
+  });
 };
