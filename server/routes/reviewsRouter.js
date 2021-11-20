725f7f8a6c0ca8dd6d44d70a684cf89a496e337f
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-11-20 15:37:56 -0800

    Set up router

---
+++ server/routes/reviewsRouter.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import {
+  getReviews,
+  getMetadata,
+  postReview,
+  markAsHelpful,
+  reportReview,
+} from '../controllers/reviewsController.js';
+
+const reviewsRouter = express.Router();
+
+/* GET requests: List Reviews and Get Review Metadata */
+
+reviewsRouter.get('/', getReviews);
+
+reviewsRouter.get('/meta', getMetadata);
+
+/* POST request: Add a Review */
+
+reviewsRouter.post('/', postReview);
+
+/* PUT requests: Mark Review as Helpful and Report Review */
+
+reviewsRouter.put('/:review_id/helpful', markAsHelpful);
+
+reviewsRouter.put('/:review_id/report', reportReview);
+
+export default reviewsRouter;
