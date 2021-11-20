1c32e61cf1220cc2a863c6a3236a6a35e6609f47
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-11-20 15:54:53 -0800

    Create db connection config example file

---
+++ database/db.example.js
@@ -0,0 +1,14 @@
+// add your details to pool and rename file to db.js
+import pkg from 'pg';
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  user: 'your-username',
+  host: 'your-host',
+  database: 'your-db-name',
+  password: 'your-password',
+  port: 3000,
+});
+
+export default pool;
