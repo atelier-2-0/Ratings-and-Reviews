7bc9cba9ffd762c2c98111a5701fb85dde3dcc0a
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-12-01 12:01:19 -0800

    Clean up for PR

--- server/model/reviewsModel.js
+++ server/model/reviewsModel.js
@@ -3,18 +3,19 @@ import pool from '../../database/db.js';
 export const listReviews = (reqQuery, callback) => {
   // need to add query to add photos and allow for sorting
   const {
-    page, count, sort, product_id,
+    page = 1, count = 5, sort, product_id,
   } = reqQuery;
 
   const query = 'SELECT review_id, rating, summary, recommend, response, body, date, reviewer_name, helpfulness FROM reviews WHERE product_id = $1 LIMIT $2;';
-  pool.query(query, [product_id, count || 5], async (error, results) => {
+
+  pool.query(query, [product_id, count], (error, results) => {
     if (error) {
       callback(error, null);
     } else {
       const rows = {
         product: product_id,
-        page: page || 0,
-        count: count || 5,
+        page,
+        count,
         results: [
           ...results.rows,
         ],
@@ -33,23 +34,11 @@ export const addReview = (data, callback) => {
   const date = new Date();
 
   const insertIntoReviews = 'INSERT INTO reviews(product_id, rating, summary, body, recommend, reviewer_name, reviewer_email, date, reported, helpfulness) values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING review_id;';
-  const insertIntoCharacteristics = 'INSERT INTO characteristics(value) values($1);';
-  const insertIntoPhotos = 'INSERT INTO photos(review_id, url) values($1, $2);';
 
   pool.query(insertIntoReviews, [product_id, rating, summary, body, recommend, name, email, date.valueOf(), false, 0], (error, results) => {
     if (error) {
       callback(error, null);
     } else {
-      // const arrayOfCharacteristics = Object.keys(characteristics);
-      // arrayOfCharacteristics.forEach((characteristic, index) => {
-      //   pool.query(insertIntoCharacteristics, [characteristic.value], (err, res) => {
-      //     if (err) {
-      //       callback(error, null);
-      //     } else if (index === arrayOfCharacteristics.length - 1) {
-      //       callback(null, `Created ${results.rows[0].review_id}`);
-      //     }
-      //   });
-      // });
       callback(null, `Created ${results.rows[0].review_id}`);
     }
   });
--- server/routes/reviewsRouter.js
+++ server/routes/reviewsRouter.js
@@ -9,15 +9,12 @@ import {
 const reviewsRouter = express.Router();
 
 /* GET requests: List Reviews and Get Review Metadata */
-
 reviewsRouter.get('/', getReviews);
 
 /* POST request: Add a Review */
-
 reviewsRouter.post('/', postReview);
 
 /* PUT requests: Mark Review as Helpful and Report Review */
-
 reviewsRouter.put('/:review_id/helpful', markAsHelpful);
 reviewsRouter.put('/:review_id/report', reportReview);
 
