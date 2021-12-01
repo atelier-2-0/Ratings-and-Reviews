6e7296f0eef7dda99bf486d07d09634155b6edd3
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-12-01 14:54:11 -0800

    Add test script with options

--- server/tests/script.js
+++ server/tests/script.js
@@ -1,5 +1,49 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 import http from 'k6/http';
-import { sleep } from 'k6';
+import {
+  jUnit,
+  textSummary,
+} from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+
+export default function () {
+  const port = 3000;
+  const productId = 61800;
+  const page = 1;
+  const count = 10;
+  const url = `http://localhost:${port}/reviews?product_id=${productId}&page=${page}&count=${count}`;
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  http.get(url, params);
+}
+
+/* *************************************************** */
+
+// export const options = { vus: 1, iterations: 5 };
+
+// export function handleSummary(data) {
+//   console.log('Preparing the end-of-test summary...');
+//   const dateStamp = new Date().toLocaleString('en-US', {
+//     timeZone: 'America/Los_Angeles',
+//   });
+//   const summaryFile = `../k6/${dateStamp}summary.json`;
+//   return {
+//     stdout: textSummary(data, { indent: ' ', enableColors: true }), // Show the text summary to stdout...
+//     summaryFile: JSON.stringify(data), // and a JSON with all the details...
+//     // And any other JS transformation of the data you can think of,
+//     // you can write your own JS helpers to transform the summary data however you like!
+//   };
+// }
+
+/* *************************************************** */
+
+// import http from 'k6/http';
+// import { sleep } from 'k6';
 
 // export const options = {
 //   vus: 10,
@@ -11,7 +55,7 @@ import { sleep } from 'k6';
 //   ],
 // };
 
-export default function () {
-  http.get('http://localhost:3000/reviews?product_id=61800');
-  sleep(1);
-}
+// export default function () {
+//   http.get('http://localhost:3000/reviews?product_id=61800');
+//   sleep(1);
+// }
