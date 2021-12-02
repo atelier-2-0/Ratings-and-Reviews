73169bb3125df64c3569517e8d56b30e4b4267fe
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-12-02 05:47:57 -0800

    Add local stress test results for get route

---
+++ server/tests/getReviews.test.js
@@ -0,0 +1,37 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable func-names */
+/* eslint-disable no-unused-expressions */
+import http from 'k6/http';
+import { check } from 'k6';
+import { Rate } from 'k6/metrics';
+
+export const errorRate = new Rate('errors');
+
+export const options = {
+  stages: [
+    { duration: '1m', target: 1 },
+    { duration: '1m', target: 10 },
+    { duration: '1m', target: 100 },
+    { duration: '1m', target: 1000 },
+    { duration: '20s', target: 0 },
+  ],
+};
+
+export default function () {
+  const port = 3000;
+  const randomPage = Math.floor(Math.random() * 11);
+  const randomCount = Math.floor(Math.random() * 20);
+  const randomProductId = Math.floor(Math.random() * 1000000) + 900000;
+
+  const url = `http://localhost:${port}/reviews?product_id=${randomProductId}&page=${randomPage}&count=${randomCount}`;
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  check(http.get(url, params), {
+    'status is 200': (r) => r.status === 200,
+  }) || errorRate.add(1);
+}
